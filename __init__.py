80f3c7c4c9dbe75e8d6bffeb308b54b0aec62ec7
cardo1011 <ricardomunoz4250@gmail.com>
2025-08-04 23:05:10 -0700

    initialized the __init__ file for my log in page using Flask

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from decouple import config
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from .admin import routes
+
+app = Flask(__name__)
+app.config.from_object(config("APP_SETTINGS"))
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+  
+# Registering blueprints
+from src.accounts.views import accounts_bp
+from src.core.views import core_bp
+
+app.register_blueprint(accounts_bp)
+app.register_blueprint(core_bp)
