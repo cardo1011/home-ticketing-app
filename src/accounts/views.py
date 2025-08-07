677267c55f7317c11ccbbd6807085c3ab77cc6b7
cardo1011 <ricardomunoz4250@gmail.com>
2025-08-06 17:00:14 -0700

    creating a register form for a new user and creating a function that uses the form in the accounts/views file

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import login_user, current_user
+
+from src import db
+from src.accounts.models import User
+from .forms import RegisterForm
 
 accounts_bp = Blueprint("accounts", __name__)
+
+@accounts_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        flash("You are already registered.", "info")
+        return redirect(url_for("core.home"))
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        user = User(email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+        flash("You registered and are now logged in. Welcome!", "success")
+
+        return redirect(url_for("core.home"))
+
+    return render_template("accounts/register.html", form=form)
