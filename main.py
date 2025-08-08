ae55374edca36549cd62e4241d092dd529ed791c
cardo1011 <ricardomunoz4250@gmail.com>
2025-08-07 19:06:24 -0700

    styled and linked the html login and register pages

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # command to run app with debug option: flask --app hello run --debug
 
-from flask import Flask , render_template
+from flask import Flask , render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    css_ = url_for('static', filename='styles.css')
+    return render_template("index.html", css_path=css_)
 
 @app.route("/register")
 def register_new_user():
-    return render_template("register.html")
+    css_ = url_for('static', filename='styles.css')
+    return render_template("register.html", css_path=css_)
 
