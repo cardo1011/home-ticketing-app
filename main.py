9d825aaa667658a437304fb20bdf3e5b9f20bbb3
cardo1011 <ricardomunoz4250@gmail.com>
2025-08-08 20:25:12 -0700

    started using sessions in order to keep track of user info but need to setup environment variable for the secret_key in order for the sessions to work

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-# command to run app with debug option: flask --app hello run --debug
+# command to run app with debug option: flask --app main.py run --debug
 
-from flask import Flask , render_template, url_for
+from flask import Flask , render_template, url_for, request, redirect, session
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+secret_key = os.environ.get("SECRET_KEY")
 
-@app.route("/")
-def main_page():
+@app.route("/login", methods=["POST", "GET"])
+def login_page():
     css_ = url_for('static', filename='styles.css')
-    return render_template("index.html", css_path=css_)
+    if request.method == "POST":
+        email = request.form["emailAddress"]
+        session["emailAddress"] = email
+        return redirect(url_for("logged_in"))
+    else:
+        return render_template("login.html", css_path=css_)
+    
+@app.route("/user")
+def logged_in():
+    if "emailAddress" in session:
+        emailAdd = session["emailAddress"]
+        return f"<h1>This is your email: {emailAdd}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/register")
 def register_new_user():
