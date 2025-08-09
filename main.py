ae485d9e65ad88c126684db8a99110f7221def6e
cardo1011 <ricardomunoz4250@gmail.com>
2025-08-09 13:24:01 -0700

    set up the secret key in order to use sessions to reflect what user is signed in currently. Going to implement a db next

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 from flask import Flask , render_template, url_for, request, redirect, session
 from dotenv import load_dotenv
 import os
+import sqlalchemy
 
 load_dotenv()
 
 app = Flask(__name__)
-secret_key = os.environ.get("SECRET_KEY")
+app.secret_key = os.getenv("SECRET_KEY")
 
 @app.route("/login", methods=["POST", "GET"])
 def login_page():
@@ -17,15 +18,22 @@ def login_page():
         session["emailAddress"] = email
         return redirect(url_for("logged_in"))
     else:
+        if "emailAddress" in session:
+            redirect(url_for(("logged_in")))
         return render_template("login.html", css_path=css_)
+
+@app.route("/logout")
+def logout():
+    session.pop("emailAddress", None)
+    return redirect(url_for("login_page"))
     
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def logged_in():
     if "emailAddress" in session:
         emailAdd = session["emailAddress"]
         return f"<h1>This is your email: {emailAdd}</h1>"
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("login_page"))
 
 @app.route("/register")
 def register_new_user():
